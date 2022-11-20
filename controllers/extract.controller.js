b03773eac98f3e1ab1d26c5c2a757321ad1d7fa6
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-20 20:12:05 -0300

    starts an array of expenses in the database

--- controllers/extract.controller.js
+++ controllers/extract.controller.js
@@ -3,6 +3,28 @@ import { db } from "../index.js";
 export async function updateExpenses(req, res) {
   const bonusOrBurden = req.body;
   const token = req.headers.authorization.replace("Bearer ", "");
-  console.log(token);
-  res.sendStatus(200);
+  const sessions = db.collection("sessions");
+  const extracts = db.collection("extracts");
+
+  try {
+    const sessionExist = await sessions.findOne({ token });
+
+    if (sessionExist) {
+      const { userId } = sessionExist;
+      const accountExist = await extracts.findOne({ userId });
+      console.log(accountExist);
+      if (accountExist) {
+        console.log("já tem po");
+        return res.sendStatus(200);
+      }
+      const arrayAccount = [bonusOrBurden];
+      const objetoDeInsersao = { userId, arrayAccount };
+      await extracts.insertOne(objetoDeInsersao);
+      return res.sendStatus(200);
+    }
+
+    return res.sendStatus(400);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
