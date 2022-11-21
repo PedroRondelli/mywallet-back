71ed6556f8142333da717eae5d85a8b074e71ae9
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-21 16:04:29 -0300

    sends user name in  get extract

--- controllers/userExtract.controller.js
+++ controllers/userExtract.controller.js
@@ -4,15 +4,17 @@ export async function getUserExtract(req, res) {
   const token = req.headers.authorization.replace("Bearer ", "");
   const sessions = db.collection("sessions");
   const extracts = db.collection("extracts");
+  const users = db.collection("users");
   try {
     const { userId } = await sessions.findOne({ token });
+    const { name } = await users.findOne({ _id:userId });
     const extractExist = await extracts.findOne({ userId });
     if (extractExist) {
       const { arrayAccount } = extractExist;
-      res.status(200).send(arrayAccount);
+      res.status(200).send({ name,arrayAccount });
       return;
     }
-    res.status(200).send([])
+    res.status(200).send({name,arrayAccount:[]});
   } catch (erro) {
     console.log(erro);
   }
